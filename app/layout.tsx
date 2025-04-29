e7406e8464c285bedf4a6f0535ebd67c07ab428d
Adarsh-oo7 <adarshsarachandran@gmail.com>
2025-04-30 00:16:31 +0530

    new

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'MUHAMMED RASHID N',
+  description: 'I am rashid',
+  generator: 'Developed by Digital Product Solutions',
 }
 
 export default function RootLayout({
