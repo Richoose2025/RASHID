f79a976e6a5f7678ce9d26cfea25a7564419aa42
Adarsh-oo7 <adarshsarachandran@gmail.com>
2025-04-29 23:46:25 +0530

    add

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  // Remove basePath as you're now using a subdomain instead of a subfolder
+  // basePath: '/Akhil-Shibu', 
+  trailingSlash: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -9,6 +13,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
